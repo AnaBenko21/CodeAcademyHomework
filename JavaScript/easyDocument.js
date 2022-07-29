3e096e5662e8f18dc1af4ea19a36f9994efcf699
AnaBenko21 <anamarijabozinovska@yahoo.com>
2022-07-29 09:40:14 +0200

    let_Info

---
+++ JavaScript/easyDocument.js
@@ -0,0 +1,16 @@
+let name1 = "Ana Marija";
+let surname1 = "Benko";
+let city1 = "Skopje";
+let job1 = "Full-Stack programmer";
+
+let user = {
+  name: name1,
+  surname: surname1,
+  city: city1,
+  job: job1,
+};
+
+console.log(user);
+function myButton() {
+  alert("this is alert");
+}
