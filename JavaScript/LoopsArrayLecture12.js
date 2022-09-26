54d3bfed17ac2b63dc031333d04a17490df50ce0
AnaBenko21 <anamarijabozinovska@yahoo.com>
2022-09-26 21:29:10 +0200

    Lecture12

---
+++ JavaScript/LoopsArrayLecture12.js
@@ -0,0 +1,18 @@
+let broevi = [1, 2, 3, 4, 5];
+broevi.splice(1, 1, 6);
+console.log(broevi);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let i = 0;
+while ((i < numbers, i++));
+console.log(numbers);
+
+let najgolemiBroevi = [12, 22, 33, 44, 55, 66];
+let najgolemBroj = 0;
+
+for (i = 0; i < najgolemiBroevi.length; i++) {
+  if (najgolemBroj < najgolemiBroevi[i]) {
+    najgolemBroj = najgolemiBroevi[i];
+  }
+}
+console.log(najgolemBroj);
--- JavaScript/leapyear.js
+++ JavaScript/leapyear.js
@@ -1,16 +1,20 @@
-function month(month) {
-  let Thirtydays = [1, 3, 5, 7, 8, 10, 12];
-  let Thirtyonedays = [4, 6, 9, 11];
-  let February = [2];
+function month() {
+  let month = prompt("Vnesi mesec:");
+  let Thirtydays = ["1", "3", "5", "7", "8", "10", "12"];
+  let Thirtyonedays = ["4", "6", "9", "11"];
+  let February = ["2"];
   if (Thirtydays.includes(month)) {
     return 30;
   } else if (Thirtyonedays.includes(month)) {
     return 31;
   } else if (February.includes(month)) {
-    return 29;
+    let year = prompt("Vnesi godina:");
+    let x = checkLeapYear(year);
+    return x;
   }
 }
-console.log(month(2));
+let a = month();
+console.log(a);
 
 // Za da se proveri preodna godina moze da se napise eden od ovie tri nacini ili site 3 zaedno  if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400))
 // function checkLeapYear(year) {
@@ -28,11 +32,15 @@ console.log(month(2));
 function checkLeapYear(year) {
   const leap = new Date(year, 1, 29).getDate() === 29;
   if (leap) {
-    console.log(year + " is a leap year");
+    return 29;
   } else {
-    console.log(year + " is not a leap year");
+    return 28;
   }
 }
 
-const year = prompt("Enter a year:");
-checkLeapYear(year);
+// const year = prompt("Enter a year:");
+// checkLeapYear(year);
+const fruits = ["Apple", "Banana", "Orange"];
+for (x of fruits) {
+  console.log(x);
+}
